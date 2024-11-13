3e979b3a9bdd769db4b72709903ad85012bf91ff
KhanhLinh <115655284+Khanhlinh952001@users.noreply.github.com>
2024-11-13 10:49:16 +0900

    Update LanguageSwitcher.tsx

--- src/components/Effects/LanguageSwitcher.tsx
+++ src/components/Effects/LanguageSwitcher.tsx
@@ -20,7 +20,7 @@ const LanguageSwitcher: FC = () => {
         className="appearance-none block w-full p-1 mt-1 pr-8 border border-gray-300 rounded-lg bg-white dark:bg-gray-800 dark:border-gray-600 text-gray-700 dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
       >
         <option value="kr">🇰🇷</option>
-        <option value="en">🇬🇧</option>
+        <option value="en">EN</option>
         <option value="vi">🇻🇳</option>
         {/* Add other languages if needed */}
       </select>
