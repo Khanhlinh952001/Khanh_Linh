ad00837e09b3fa3bdeb27540220a56c16cba89f9
Khanhlinh952001 <vokhanhlinh952001@gmail.com>
2024-11-22 02:42:11 +0900

    add CV KR

--- src/components/UI/About.tsx
+++ src/components/UI/About.tsx
@@ -2,10 +2,12 @@
 "use client";
 import { useTranslations } from "next-intl";
 import { FiArrowDownCircle } from "react-icons/fi";
-
+import { useParams } from "next/navigation";
 export const AboutPage = () => {
   const t = useTranslations("Info");
-
+  const params = useParams();
+  const locale = params.locale;
+  console.log(locale);
   return (
     <div className="container mx-auto max-w-7xl pt-24 px-4">
       <div className="flex flex-col lg:flex-row items-center justify-between gap-8 py-12">
@@ -37,17 +39,18 @@ export const AboutPage = () => {
             {t('bio')}
           </p>
           <div className="flex justify-center lg:justify-start">
-            <a
-              download="/CV-KhanhLinh.pdf"
-              href="/CV-KhanhLinh.pdf"
-              className="font-medium flex justify-center items-center w-36 sm:w-48 mt-8 mb-6 sm:mb-0 border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
-              aria-label="Download CV"
-            >
-              <FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sm:w-6 sm:h-6 duration-100" />
-              <span className="text-sm sm:text-base font-general-medium duration-100">
-                Download CV
-              </span>
-            </a>
+          <a
+  download={`${locale}/CV-KhanhLinh.pdf`}
+  href={`/${locale}/CV-KhanhLinh.pdf`}
+  className="font-medium flex justify-center items-center w-36 sm:w-48 mt-8 mb-6 sm:mb-0 border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
+  aria-label="Download CV"
+>
+  <FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sm:w-6 sm:h-6 duration-100" />
+  <span className="text-sm sm:text-base font-general-medium duration-100">
+    {locale === "kr" ? "다운로드 CV" : locale === "vi" ? "Tải xuống CV" : "Download CV"}
+  </span>
+</a>
+
           </div>
         </div>
       </div>
